c5342f8e365a5ce4c9ae95717a1b5f7e5fc51b43
unknown <michaelcmatteis@gmail.com>
2025-02-04 14:50:18 -0600

    trying diffrent package

--- routes/teaUserRouter.js
+++ routes/teaUserRouter.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const db = require("../Data/Database")
 const router = express.Router()
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 
 const Hash = 10
